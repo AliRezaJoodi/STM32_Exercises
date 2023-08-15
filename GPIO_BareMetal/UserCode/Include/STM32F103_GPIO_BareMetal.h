d0efb4d732f54128de95fcded19c6062ff51d617
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2023-08-15 05:40:02 +0330

    Update

--- GPIO_BareMetal/UserCode/Include/STM32F103_GPIO_BareMetal.h
+++ GPIO_BareMetal/UserCode/Include/STM32F103_GPIO_BareMetal.h
@@ -3,38 +3,46 @@
 #ifndef _GPIO_INCLUDED
     #define _GPIO_INCLUDED
 
-// Pin Direction Selection
-#define INPUT_MODE               											0b00UL
-#define OUTPUT_MODE_10MHz        											0b01UL
-#define OUTPUT_MODE_2MHz         											0b10UL
-#define OUTPUT_MODE_50MHz        											0b11UL
-#define GPIO_ConfigurePinDirection(GPIOx,BIT,MODE)    if(BIT<=7){\
-                                                      (GPIOx->CRL)= ((GPIOx->CRL) & ~(0b11UL<<(BIT*4))) | ((MODE&0b11UL)<<(BIT*4));}\
-                                                      else{\
-                                                      (GPIOx->CRH)= ((GPIOx->CRH) & ~(0b11UL<<((BIT-8)*4))) | ((MODE&0b11UL)<<((BIT-8)*4));}
+// Serial Wire JTAG Configuration
+#define FULL_SWJ												0b000U
+#define FULL_SWJ_WITHOUT_NJTRST					0b001U
+#define JTAG_DISABLED_AND_SWD_ENABLED		0b010U
+#define JTAG_DISABLED_AND_SWD_DISABLED	0b100U
+#define ConfigureSerialWireDebugPort(MODE) \
+	AFIO->MAPR= (AFIO->MAPR & ~AFIO_MAPR_SWJ_CFG) | (MODE<<AFIO_MAPR_SWJ_CFG_Pos);
 
-// Input Mode Selection
-#define ANALOG_MODE               										0b00UL
-#define FLOATING_INPUT        												0b01UL
-#define INPUT_WITH_PULLUP_PULLDOWN        						0b10UL
-#define GPIO_ConfigureInputMode(GPIOx,BIT,MODE)				if(BIT<=7){\
-																											(GPIOx->CRL)= ((GPIOx->CRL) & ~(0b11UL<<((BIT*4)+2))) | ((MODE&0b11UL)<<((BIT*4)+2));}\
-																											else{\
-																											(GPIOx->CRH)= ((GPIOx->CRH) & ~(0b11UL<<(((BIT-8)*4)+2))) | ((MODE&0b11UL)<<(((BIT-8)*4)+2));}
+// Pin Direction Configuration
+#define INPUT_MODE          0b00UL
+#define OUTPUT_MODE_10MHz   0b01UL
+#define OUTPUT_MODE_2MHz    0b10UL
+#define OUTPUT_MODE_50MHz		0b11UL
+#define GPIO_ConfigurePinDirection(GPIOx,BIT,MODE) \
+	if(BIT<=7){(GPIOx->CRL)= ((GPIOx->CRL) & ~(0b11UL<<(BIT*4))) | ((MODE&0b11UL)<<(BIT*4));}\
+		else{(GPIOx->CRH)= ((GPIOx->CRH) & ~(0b11UL<<((BIT-8)*4))) | ((MODE&0b11UL)<<((BIT-8)*4));}
 
-//Pull-up or Pull-down Selection
-#define PULLDOWN        															0
-#define PULLUP        																1
-#define GPIO_ConfigurePullUpOrPullDown(GPIOx,BIT,MODE)	(GPIOx->ODR) |= ((MODE&0b1UL) << BIT);	
+// Input Mode Configuration
+#define ANALOG_MODE               	0b00UL
+#define FLOATING_INPUT							0b01UL
+#define INPUT_WITH_PULLUP_PULLDOWN	0b10UL
+#define GPIO_ConfigureInputMode(GPIOx,BIT,MODE) \
+		if(BIT<=7){(GPIOx->CRL)= ((GPIOx->CRL) & ~(0b11UL<<((BIT*4)+2))) | ((MODE&0b11UL)<<((BIT*4)+2));}\
+			else{(GPIOx->CRH)= ((GPIOx->CRH) & ~(0b11UL<<(((BIT-8)*4)+2))) | ((MODE&0b11UL)<<(((BIT-8)*4)+2));}
 
-// Output Mode Selection
-#define GENERAL_PURPOSE_OUTPUT_PUSHPULL               0b00UL
-#define GENERAL_PURPOSE_OUTPUT_OPENDRAIN        			0b01UL
-#define ALTERNATE_FUNCTION_OUTPUT_PUSHPULL        		0b10UL
-#define ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN        		0b11UL
-#define GPIO_ConfigureOutputMode(GPIOx,BIT,MODE)			(GPIOx->CRL)= ((GPIOx->CRL) & ~(0b11UL<<((BIT*4)+2))) | ((MODE&0b11UL)<<((BIT*4)+2));
+//Pull-up or Pull-down Configuration
+#define PULLDOWN	0
+#define PULLUP		1
+#define GPIO_ConfigurePullUpOrPullDown(GPIOx,BIT,MODE) \
+	(GPIOx->ODR) |= ((MODE&0b1UL) << BIT);	
 
-// Commands																												
+// Output Mode Configuration
+#define GENERAL_PURPOSE_OUTPUT_PUSHPULL  		0b00UL
+#define GENERAL_PURPOSE_OUTPUT_OPENDRAIN 		0b01UL
+#define ALTERNATE_FUNCTION_OUTPUT_PUSHPULL	0b10UL
+#define ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN	0b11UL
+#define GPIO_ConfigureOutputMode(GPIOx,BIT,MODE) \
+	(GPIOx->CRL)= ((GPIOx->CRL) & ~(0b11UL<<((BIT*4)+2))) | ((MODE&0b11UL)<<((BIT*4)+2));
+
+// Other Commands																												
 #define GPIO_GetInputPin(GPIOx,BIT) 									(((GPIOx->IDR) & (0b1UL<<BIT)) >> BIT)
 #define GPIO_GetInputPort(GPIOx)											(GPIOx->IDR)
 																												
