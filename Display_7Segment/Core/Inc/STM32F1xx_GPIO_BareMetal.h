6e13ca890784933630790bfc0203cf415fd0be2d
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2023-08-21 00:12:17 +0330

    Initial Entry

---
+++ Display_7Segment/Core/Inc/STM32F1xx_GPIO_BareMetal.h
@@ -0,0 +1,91 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifndef _GPIO_INCLUDED
+    #define _GPIO_INCLUDED
+
+// Serial Wire JTAG Configuration
+#define FULL_SWJ												0b000U
+#define FULL_SWJ_WITHOUT_NJTRST					0b001U
+#define JTAG_DISABLED_AND_SWD_ENABLED		0b010U
+#define JTAG_DISABLED_AND_SWD_DISABLED	0b100U
+#define ConfigureSerialWireDebugPort(MODE) \
+	AFIO->MAPR= (AFIO->MAPR & ~AFIO_MAPR_SWJ_CFG) | (MODE<<AFIO_MAPR_SWJ_CFG_Pos);
+
+// Pin Direction Configuration
+#define INPUT_MODE          0b00UL
+#define OUTPUT_MODE_10MHz   0b01UL
+#define OUTPUT_MODE_2MHz    0b10UL
+#define OUTPUT_MODE_50MHz		0b11UL
+#define ConfigurePinForDirection(GPIOx,PIN,MODE) \
+	if(PIN<=7){(GPIOx->CRL)= ((GPIOx->CRL) & ~(0b11UL<<(PIN*4))) | ((MODE&0b11UL)<<(PIN*4));}\
+		else{(GPIOx->CRH)= ((GPIOx->CRH) & ~(0b11UL<<((PIN-8)*4))) | ((MODE&0b11UL)<<((PIN-8)*4));}
+
+// Input Mode Configuration
+#define ANALOG_MODE               	0b00UL
+#define FLOATING_INPUT							0b01UL
+#define INPUT_WITH_PULLUP_PULLDOWN	0b10UL
+#define ConfigurePinForInputMode(GPIOx,PIN,MODE) \
+		if(PIN<=7){(GPIOx->CRL)= ((GPIOx->CRL) & ~(0b11UL<<((PIN*4)+2))) | ((MODE&0b11UL)<<((PIN*4)+2));}\
+			else{(GPIOx->CRH)= ((GPIOx->CRH) & ~(0b11UL<<(((PIN-8)*4)+2))) | ((MODE&0b11UL)<<(((PIN-8)*4)+2));}
+
+//Pull-up or Pull-down Configuration
+#define PULLDOWN	0
+#define PULLUP		1
+#define ConfigurePinForPullUpOrDown(GPIOx,PIN,MODE) \
+	GPIOx->ODR= (GPIOx->ODR & ~(0b1UL << PIN)) | ((MODE&0b1UL) << PIN);
+
+// Output Mode Configuration
+#define GENERAL_PURPOSE_OUTPUT_PUSHPULL  		0b00UL
+#define GENERAL_PURPOSE_OUTPUT_OPENDRAIN 		0b01UL
+#define ALTERNATE_FUNCTION_OUTPUT_PUSHPULL	0b10UL
+#define ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN	0b11UL
+#define ConfigurePinForOutputMode(GPIOx,PIN,MODE) \
+	if(PIN<=7){(GPIOx->CRL)= ((GPIOx->CRL) & ~(0b11UL<<((PIN*4)+2))) | ((MODE&0b11UL)<<((PIN*4)+2));}\
+		else{(GPIOx->CRH)= ((GPIOx->CRH) & ~(0b11UL<<(((PIN-8)*4)+2))) | ((MODE&0b11UL)<<(((PIN-8)*4)+2));}
+																										
+#define GetPinFromInput(GPIOx,PIN) \
+	(((GPIOx->IDR) >> PIN) & 0b1UL)
+#define Get2PinFromInput(GPIOx,FROM) \
+	((GPIOx->IDR >> FROM) & 0b11UL)
+#define Get3PinFromInput(GPIOx,FROM) \
+	((GPIOx->IDR >> FROM) & 0b111UL)
+#define Get4PinFromInput(GPIOx,FROM) \
+	((GPIOx->IDR >> FROM) & 0b1111UL)
+#define GetPortFromInput(GPIOx) \
+	(GPIOx->IDR)
+																												
+#define SetPinFromOutputWithBSRR(GPIOx,PIN) \
+	GPIOx->BSRR = 0b1UL<<PIN;	//Fast
+#define SetPinFromOutputWithODR(GPIOx,PIN) \
+	(GPIOx->ODR) |= (0b1UL<<PIN);
+#define SetPinFromOutput(GPIOx,PIN) \
+	SetPinFromOutputWithBSRR(GPIOx,PIN)
+
+#define ResetPinFromOutputWithBRR(GPIOx,PIN) \
+	(GPIOx->BRR) = (0b1UL<<PIN);	//Fast
+#define ResetPinFromOutputWithODR(GPIOx,PIN) \
+	(GPIOx->ODR) &= ~(0b1U<<PIN);
+#define ResetPinFromOutputWithBSRR(GPIOx,PIN) \
+	(GPIOx->BSRR) = (0b1U<<(PIN+16));
+#define ResetPinFromOutput(GPIOx,PIN) \
+	ResetPinFromOutputWithBRR(GPIOx,PIN)
+
+#define TogglePinFromOutput(GPIOx,PIN) \
+	(GPIOx->ODR) ^= ( 0b1UL<<PIN);
+
+#define WritePinFromOutput(GPIOx,PIN,STATUS) \
+	GPIOx->ODR= (GPIOx->ODR & ~(0b1UL << PIN)) | (((STATUS)&0b1UL) << PIN);
+#define Write2PinFromOutput(GPIOx,FROM,VALUE) \
+	GPIOx->ODR= ((GPIOx->ODR) & ~(0b11UL << FROM)) | ((VALUE&0b11UL) << FROM);
+#define Write3PinFromOutput(GPIOx,FROM,VALUE) \
+	GPIOx->ODR= ((GPIOx->ODR) & ~(0b111UL << FROM)) | ((VALUE&0b111UL) << FROM);
+#define Write4PinFromOutput(GPIOx,FROM,VALUE) \
+	GPIOx->ODR= ((GPIOx->ODR) & ~(0b1111UL << FROM)) | ((VALUE&0b1111UL) << FROM);
+#define WritePortFromOutput(GPIOx,VALUE) \
+	(GPIOx->ODR) = VALUE;
+
+#define GetPinFromOutput(GPIOx,PIN) \
+	(((GPIOx->ODR) >> PIN) & 0b1UL)
+
+#endif
+
---
+++ Display_7Segment/Core/Src/main.c
@@ -0,0 +1,235 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "STM32F1xx_BUS_BareMetal.h"
+#include "STM32F1xx_GPIO_BareMetal.h"
+#include "Utility.h"
+#include "Display_7Segment.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+uint8_t table_7segment_[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+uint8_t i =0; 
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+
+/* USER CODE BEGIN PFP */
+//**************************************
+void Display_7Segment(unsigned int value){
+			value=_ConvertNumberTo7SegmentData(value);
+			if(ACTIVATE_SEGMENT==0){value= _ToggleValue(value);}
+			_DriveDataOn7Segment(value);
+			WritePinFromOutput(DIGIT0_GPIO,DIGIT0_PIN,ACTIVATE_DIGIT);
+}
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void){
+  /* USER CODE BEGIN 1 */
+	char status=1;
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_AFIO);
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+
+  /* System interrupt init*/
+  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+
+  /* SysTick_IRQn interrupt configuration */
+  NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),15, 0));
+
+  /** DISABLE: JTAG-DP Disabled and SW-DP Disabled
+  */
+  LL_GPIO_AF_DisableRemap_SWJ();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+	Configure7Segment;
+  /* USER CODE END 2 */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	Display_7Segment(0);
+	
+  while(1){
+    /* USER CODE END WHILE */
+    /* USER CODE BEGIN 3 */
+		//if ((((((LL_GPIO_ReadInputPort(GPIOB)) >> 0) & 0b1UL)) == 0) && status==1){
+		if(GetPinFromInput(GPIOB,0) == 0 && status==1){
+			status=0;
+			i++; if(i>9){i=0;}
+			Display_7Segment(i);
+		}
+		if(GetPinFromInput(GPIOB,0)){status=1;}
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+  while(LL_FLASH_GetLatency()!= LL_FLASH_LATENCY_0)
+  {
+  }
+  LL_RCC_HSI_SetCalibTrimming(8);
+  LL_RCC_HSI_Enable();
+
+   /* Wait till HSI is ready */
+  //while(LL_RCC_HSI_IsReady() != 1)
+  {
+
+  }
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+
+   /* Wait till System clock is ready */
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI)
+  {
+
+  }
+  LL_Init1msTick(8000000);
+  LL_SetSystemCoreClock(8000000);
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOA);
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_GPIOB);
+
+  /**/
+	/*
+  LL_GPIO_ResetOutputPin(GPIOA, LL_GPIO_PIN_0|LL_GPIO_PIN_1|LL_GPIO_PIN_2|LL_GPIO_PIN_3
+                          |LL_GPIO_PIN_4|LL_GPIO_PIN_5|LL_GPIO_PIN_6);
+	*/
+	
+  /**/
+	/*
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_0|LL_GPIO_PIN_1|LL_GPIO_PIN_2|LL_GPIO_PIN_3
+                          |LL_GPIO_PIN_4|LL_GPIO_PIN_5|LL_GPIO_PIN_6;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	*/
+
+  /**/
+  GPIO_InitStruct.Pin = BUTTON_INCR_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(BUTTON_INCR_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
