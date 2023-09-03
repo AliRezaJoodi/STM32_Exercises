43085d349a077c2019af1787e4d3d7c0475945c6
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2023-09-03 16:17:39 +0330

    Update

--- Interrupt_BareMetal/Core/Src/main.c
+++ Interrupt_BareMetal/Core/Src/main.c
@@ -29,22 +29,35 @@ int main(void){
 
 //***************************************************
 void SystemClock_Config(void){
-  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
-  while(LL_FLASH_GetLatency()!= LL_FLASH_LATENCY_0){}
-  LL_RCC_HSI_SetCalibTrimming(16);
-  LL_RCC_HSI_Enable();
-
+  //LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+	ConfigureLatencyForFLASH(FLASH_LATENCY0);
+  //while(LL_FLASH_GetLatency()!= LL_FLASH_LATENCY_0){}
+	WaitTillRightLatency(FLASH_LATENCY0);
+  //LL_RCC_HSI_SetCalibTrimming(16);
+	SetCalibTrimmingFromHSI(16);
+  //LL_RCC_HSI_Enable();
+	EnableOrDisableClockFromHSI(1);
    /* Wait till HSI is ready */
-  while(LL_RCC_HSI_IsReady() != 1){}
-  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
-  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
-  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
-  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
-
+  //while(LL_RCC_HSI_IsReady() != 1){}
+	WaitTillStableClockSourceFromHSI
+		
+  //LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+	ConfigurePrescalerForAHB(AHB_DIV1);
+  //LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+	ConfigurePrescalerForAPB1(APB1_DIV1);
+  //LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+	ConfigurePrescalerForAPB2(APB2_DIV1);
+  //LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+	ConfigureSystemClockSource(SYSCLK_HSI);
    /* Wait till System clock is ready */
-  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI){}
+  //while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI){}
+	//while(GetSystemClockSource != SYSCLK_HSI){}
+	WaitTillRightClockSource(SYSCLK_HSI);
+	
+	SystemCoreClockUpdate();	
   LL_Init1msTick(8000000);
-  LL_SetSystemCoreClock(8000000);
+  //LL_SetSystemCoreClock(8000000);
+	SetDirectlySystemCoreClock(8000000);
 }
 
 //**********************************************************
