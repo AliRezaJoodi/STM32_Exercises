2a6ff03e5cc6478f54d0de137aafee22d2f2c5b1
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2023-09-04 23:49:34 +0330

    Update

--- GPIO_BareMetal/Core/Inc/STM32F1xx_System_BareMetal.h
+++ GPIO_BareMetal/Core/Inc/STM32F1xx_System_BareMetal.h
@@ -3,6 +3,11 @@
 #ifndef _SYSTEM_INCLUDED
     #define _SYSTEM_INCLUDED
 
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+
 #define FLASH_LATENCY0	0b000U	// Zero wait state, if 0 < SYSCLK <= 24MHz
 #define FLASH_LATENCY1	0b001U	// One wait state, if 24MHz < SYSCLK <= 48MHz
 #define FLASH_LATENCY2	0b010U	// Two wait states, if 48MHz < SYSCLK <= 72MHz
@@ -16,5 +21,10 @@
 // HCLK Clock Frequency		
 #define SetDirectlySystemCoreClock(HZ) \
 	SystemCoreClock = HZ;
+
+
+#ifdef __cplusplus
+    }
+#endif
 
 #endif
