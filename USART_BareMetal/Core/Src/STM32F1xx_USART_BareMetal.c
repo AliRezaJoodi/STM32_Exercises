e74192a806ea75898304087e3667ab5ebfbec489
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2023-09-30 03:31:20 +0330

    Update

--- USART_BareMetal/Core/Src/STM32F1xx_USART_BareMetal.c
+++ USART_BareMetal/Core/Src/STM32F1xx_USART_BareMetal.c
@@ -1,7 +1,12 @@
 // GitHub Account: GitHub.com/AliRezaJoodi
 
-#include "main.h"
 #include "STM32F1xx_USART_BareMetal.h"
+
+//*****************************************
+void USART1_ConfigureNVIC(void){
+	NVIC_SetPriority(USART1_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),0, 0));
+  NVIC_EnableIRQ(USART1_IRQn);
+}
 
 //********************************
 /*
@@ -14,8 +19,6 @@ void USART1_PutChar(char data){
 //********************************
 void USART1_PutString(char *str){
   while(*str != 0){
-		//USART1_TransmitData(*str);
-		//while(!USART1_TX_GeTransmitStatus){};
 		USART1_PutChar(*str);
     ++str;
   }
@@ -24,27 +27,31 @@ void USART1_PutString(char *str){
 //********************************
 void USART1_PutStringFromFlash(const char *str){
   while(*str != 0){
-		//USART1_TransmitData(*str);
-    //while(!USART1_TX_GeTransmitStatus){};
 		USART1_PutChar(*str);
 		++str;
   }
 }
 
-//*****************************************
+//********************************
 /*
-void USART1_PutInteger(int number){
-	int n=number;
-	while(n != 0){
-		USART1_PutNumber(n - (n/10));
-		n=n/10;
-	}
+void USART2_PutChar(char data){
+	USART2_TransmitData(data);
+	while(!USART2_TX_GeTransmitStatus){};
 }
 */
 
-//*****************************************
-void USART1_ConfigureNVIC(void){
-	NVIC_SetPriority(USART1_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),0, 0));
-  NVIC_EnableIRQ(USART1_IRQn);
+//********************************
+void USART2_PutString(char *str){
+  while(*str != 0){
+		USART2_PutChar(*str);
+    ++str;
+  }
 }
 
+//********************************
+void USART2_PutStringFromFlash(const char *str){
+  while(*str != 0){
+		USART2_PutChar(*str);
+		++str;
+  }
+}
--- USART_BareMetal/Core/Src/main.c
+++ USART_BareMetal/Core/Src/main.c
@@ -1,6 +1,6 @@
 // GitHub Account: GitHub.com/AliRezaJoodi
 
-#include "main.h"
+#include <stdio.h>
 
 #include "Utility.h"
 #include "STM32F1xx_System_BareMetal.h"
@@ -8,64 +8,63 @@
 #include "STM32F1xx_BUS_BareMetal.h"
 #include "STM32F1xx_GPIO_BareMetal.h"
 #include "STM32F1xx_USART_BareMetal.h"
+#include "_USART.h"
 
-uint8_t j=0;
+#ifndef NVIC_PRIORITYGROUP_0
+	#define NVIC_PRIORITYGROUP_0	((uint32_t)0x00000007) /*!< 0 bit  for pre-emption priority, 4 bits for subpriority */
+	#define NVIC_PRIORITYGROUP_1	((uint32_t)0x00000006) /*!< 1 bit  for pre-emption priority, 3 bits for subpriority */
+	#define NVIC_PRIORITYGROUP_2	((uint32_t)0x00000005) /*!< 2 bits for pre-emption priority, 2 bits for subpriority */
+	#define NVIC_PRIORITYGROUP_3	((uint32_t)0x00000004) /*!< 3 bits for pre-emption priority, 1 bit  for subpriority */
+	#define NVIC_PRIORITYGROUP_4	((uint32_t)0x00000003) /*!< 4 bits for pre-emption priority,                                                                0 bit  for subpriority */
+#endif
+
+char usart1_txt[16]= "";
 volatile char usart1_task=0;
-char txt[16]= "";
 
 void ConfigureSystemClock(void);
-static void ConfigureUSART1(void);
-void USART1_IRQHandler(void);
+void ClearString(char *str);
 
 int main(void){
-  /* System interrupt init*/
-  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
-	
+  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4); // System interrupt init
 	BUS_PWR_EnableOrDisable(1);
 	BUS_AFIO_EnableOrDisable(1);
 	AFIO_SetSerialWireDebugPort(FULL_SWJ);
-
   ConfigureSystemClock();
-  ConfigureUSART1();
+  USART1_Configure();
 	//LL_mDelay(500);
 
-	USART1_PutChar('A'); USART1_PutNewLine;
-	//USART1_PutNumber(9); USART1_PutNewLine;
+	USART1_PutChar('A');USART1_PutNewLine;	
 	
-	USART1_PutString("Test1\r\n");
+	USART1_PutString("Test1\r\n");	
 	
 	const char txt2[]= "Test2\r\n";
 	USART1_PutStringFromFlash(txt2);
 	
 	char txt3[]= "Test3\r\n";
+	ClearString(txt3);
 	USART1_PutString(txt3);
 	
-	//USART1_PutInteger(123);
+	
+	sprintf(txt3, "a=%0.3f", 12.3456);
+	USART1_PutString(txt3); USART1_PutNewLine;
 	
   while(1){
 		if(usart1_task==1){
 			usart1_task=0;
-			USART1_PutString(txt); USART1_PutNewLine;
+			
+			USART1_PutString(usart1_txt);
+			USART1_PutNewLine;
+			ClearString(usart1_txt);
 		}	
   }
 }
 
-//*****************************************
-void USART1_IRQHandler(void){
-	static char c;
-	static unsigned char i=0;
-	
-	if(USART1_RX_GetReceiveFlag && USART1_RX_INT_GetEnableStatus){
-		if(c==13){
-			do{--i; txt[i]=0;} while(i);
-		}
-				
-		c = USART1_ReceiveData_8Bits;
-		USART1_TransmitData(c);
-		
-		if(32<=c && c<127){txt[i]=c; ++i;}
-			else if(c==13){i=0; usart1_task=1;}
-	}
+//********************************
+void ClearString(char *str){
+  while(*str != 0){
+		*str=0;
+    ++str;
+  }
 }
 
 //****************************************************
@@ -83,41 +82,5 @@ void ConfigureSystemClock(void){
 	System_SetCoreClockFrequency(8000000);
 }
 
-//****************************************************
-//PA9 -> USART1_TX
-//PA10 -> USART1_RX
-static void ConfigureUSART1(void){
-	BUS_GPIOA_EnableOrDisable(1);
-	GPIO_SetInputOrOutputMode(GPIOA,9, MODE_OUTPUT_50MHz);
-	GPIO_OutputMode_SetGeneralOrAlternateOutput(GPIOA,9, OUTPUT_AFIO);
-	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOA,9, OUTPUT_PUSHPULL);
-	GPIO_SetInputOrOutputMode(GPIOA,10, MODE_INPUT);
-	GPIO_InputMode_SetInputType(GPIOA,10, INPUT_FLOATING);
-	
-	USART1_ConfigureNVIC();
-	BUS_USART1_EnableOrDisable(1);
-	
-  LL_USART_InitTypeDef USART_InitStruct = {0};
-  USART_InitStruct.BaudRate = 9600;
-  //USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
-  //USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
-  //USART_InitStruct.Parity = LL_USART_PARITY_NONE;
-  //USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
-  //USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
-  USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
-  LL_USART_Init(USART1, &USART_InitStruct);
 
-	USART1_SetMode(MODE_ASYNCRON);
-	USART1_SetDataBits(DATABITS_8B);
-	USART1_SetParity(PARITY_NONE);
-	USART1_SetStopBits(STOPBITS_1);
-	USART1_TX_EnableOrDisable(1);
-	USART1_TX_INT_EnableOrDisable(0);
-	USART1_RX_EnableOrDisable(1);
-	USART1_RX_INT_EnableOrDisable(1);
-	USART1_CTS_EnableOrDisable(0);
-	USART1_CTS_INT_EnableOrDisable(0);
-	USART1_RTS_EnableOrDisable(0);	
-	USART1_EnableOrDisable(1);
-}
 
