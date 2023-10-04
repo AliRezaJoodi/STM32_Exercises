deb07830c6602330a8abbdda7578702a6062520f
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2023-10-04 04:36:22 +0330

    Initial Entry

---
+++ Display_7Segment_4Digit/UserCode/Delay.h
@@ -0,0 +1,20 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifndef _DELAY_INCLUDED
+    #define _DELAY_INCLUDED
+
+#ifdef __cplusplus
+	extern "C" {
+#endif 
+
+
+#define _DELAY_LAG	8
+
+void Delay_us(unsigned int us);
+void Delay_ms(unsigned int ms);
+
+#ifdef __cplusplus
+	}
+#endif 
+
+#endif
---
+++ Display_7Segment_4Digit/UserCode/Display_7Segment_1Digit.c
@@ -0,0 +1,51 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#include "Display_7Segment.h"
+
+#ifndef _DIGITS_PORT
+    #define _DIGITS_PORT 
+		
+		#define DIGIT_ON      0
+		
+		#define DIGIT0_GPIO         GPIOA
+    #define DIGIT0_PIN          15
+#endif
+
+#define _ConfigureDigitsPins \
+	_ConfigurePinFor7Segment(DIGIT0_GPIO, DIGIT0_PIN);\
+
+#define _OffDigitsPins \
+	GPIO_WritePin(DIGIT0_GPIO, DIGIT0_PIN, !DIGIT_ON);\
+
+
+//0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+//A , B , C , D , E , F,
+// - , Dp
+const unsigned char table_7segment[18]={
+    0b00111111 , 0b00000110 , 0b01011011 , 0b01001111 , 0b01100110 , 0b01101101 , 0b01111101 , 0b00000111 , 0b01111111 , 0b01101111,
+    0b01110111 , 0b01111100 , 0b00111001 , 0b01011110 , 0b01111001 , 0b01110001 ,
+    0b01000000 , 0b10000000,
+ };
+
+
+//***************************************************
+void SevenSegment_1Digit_Configuration(void){
+	BUS_GPIOA_EnableOrDisable(1);
+	
+	_ConfigureSegmentsPins;
+	_OffSegmentsPins;
+	
+	_ConfigureDigitsPins;
+	_OffDigitsPins;
+}
+
+//**************************************
+void SevenSegment_1Digit_Number(unsigned char value){
+	#if SEGMENT_ON == 0
+		value = ~table_7segment[value];
+	#else
+		value = table_7segment[value];
+	#endif
+	_DriveDataOn7Segment(value);
+	GPIO_WritePin(DIGIT0_GPIO, DIGIT0_PIN, DIGIT_ON);
+}
