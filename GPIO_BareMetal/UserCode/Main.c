5eeb149203ffb3eed3749186e996a1cc0236f5ca
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2024-09-17 00:21:36 +0330

    Update

--- GPIO_BareMetal/UserCode/Main.c
+++ GPIO_BareMetal/UserCode/Main.c
@@ -1,107 +1,144 @@
 // GitHub Account: GitHub.com/AliRezaJoodi
 
-#include "Utility.h"
-#include "STM32F1xx_System_BareMetal.h"
-#include "STM32F1xx_RCC_BareMetal.h"
-#include "STM32F1xx_BUS_BareMetal.h"
-#include "STM32F1xx_GPIO_BareMetal.h"
+#include <stm32f1xx_bm_system.h>
+#include <stm32f1xx_bm_rcc.h>
+#include <stm32f1xx_bm_bus.h>
+#include <stm32f1xx_bm_gpio.h>
+#include <delay.h>
+#include <utility.h>
 
-void ConfigureRelays(void);
-void ConfigureButtons(void);
-void ConfigureBcdInput(void);
-void ConfigureBcdDisplay(void);
+#include "_SystemClock.h"
+void LED_Config(void);
+void Buttons_Config(void);
+void BcdInput_Config(void);
+void BcdDisplay_Config(void);
+void Button1(void);
+void Button2(void);
 
 int main(void){
-	char status0=0;
-
+	uint32_t value=0;
+	
   NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4); // System interrupt init
   NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(),15, 0)); // SysTick_IRQn interrupt configuration
 	
 	BUS_PWR_EnableOrDisable(1);
 	BUS_AFIO_EnableOrDisable(1);
-	AFIO_SetSerialWireDebugPort(FULL_SWJ);
+	GPIO_SWJ_SetDebugInterfaces(SWD_ON__JTAG_OFF);
 
-  SystemClock_Configuration();
-	ConfigureButtons();
-	ConfigureRelays();
-	ConfigureBcdInput();
-	ConfigureBcdDisplay();
+  SystemClock_Config();
+
+	Buttons_Config();
+	LED_Config();
+	BcdInput_Config();
+	BcdDisplay_Config();
 	
   while(1){
-		GPIO_WritePin(GPIOA,3, GPIO_GetPin(GPIOA,0));	
+		Button1();
+		Button2();
 		
-		if(GPIO_GetPin(GPIOA,1)==1){GPIO_SetPin(GPIOA,4);}
-		if(GPIO_GetPin(GPIOA,2)==0){GPIO_ResetPin(GPIOA,4);}
-		
-		uint32_t value= GPIO_Get4Pin(GPIOA,5);
-		GPIO_Write4Pin(GPIOA,9, value);
+		value= GPIO_Get4Pin(GPIOA,4);
+		GPIO_Write4Pin(GPIOB,12, value);
   }
 }
 
-//**************************************
-void ConfigureBcdInput(void){
-	GPIO_SetInputOrOutputMode(GPIOA,5, MODE_INPUT);
-	GPIO_InputMode_SetInputType(GPIOA,5, INPUT_FLOATING);
+//**********************************************
+void Button1(void){
+		static char status=1;
 	
-	GPIO_SetInputOrOutputMode(GPIOA,6, MODE_INPUT);
-	GPIO_InputMode_SetInputType(GPIOA,6, INPUT_FLOATING);
-	
-	GPIO_SetInputOrOutputMode(GPIOA,7, MODE_INPUT);
-	GPIO_InputMode_SetInputType(GPIOA,7, INPUT_FLOATING);
-	
-	GPIO_SetInputOrOutputMode(GPIOA,8, MODE_INPUT);
-	GPIO_InputMode_SetInputType(GPIOA,8, INPUT_FLOATING);
+    if(!GPIO_GetPin(GPIOA,0) && status==1){
+				Delay_ms(30);
+				if(!GPIO_GetPin(GPIOA,0)){
+					status=0;
+					GPIO_TogglePin(GPIOC,13);
+				}
+		}
+    else if(GPIO_GetPin(GPIOA,0) && status==0){
+			Delay_ms(30);
+			if(GPIO_GetPin(GPIOA,0)){
+				status=1;
+			}	
+		}		
+}
+
+//**********************************************
+void Button2(void){
+	static char status=0;
+
+		if(GPIO_GetPin(GPIOA,1) && status==0){
+				Delay_ms(30);
+				if(GPIO_GetPin(GPIOA,1)){
+					status=1;
+					GPIO_SetPin(GPIOC,13);
+				}
+		}
+		else if(!GPIO_GetPin(GPIOA,1) && status==1){
+			Delay_ms(30);
+			if(!GPIO_GetPin(GPIOA,1)){
+				status=0;
+			}	
+		}	
 }
 
 //**************************************
-void ConfigureBcdDisplay(void){
-	GPIO_SetInputOrOutputMode(GPIOA,9, MODE_OUTPUT_2MHz);
-	GPIO_OutputMode_SetGeneralOrAlternateOutput(GPIOA,9, OUTPUT_GPIO);
-	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOA,9, OUTPUT_PUSHPULL);
-	GPIO_ResetPin(GPIOA,9);
-	
-	GPIO_SetInputOrOutputMode(GPIOA,10, MODE_OUTPUT_2MHz);
-	GPIO_OutputMode_SetGeneralOrAlternateOutput(GPIOA,10, OUTPUT_GPIO);
-	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOA,10, OUTPUT_PUSHPULL);
-	GPIO_ResetPin(GPIOA,10);
-	
-	GPIO_SetInputOrOutputMode(GPIOA,11, MODE_OUTPUT_2MHz);
-	GPIO_OutputMode_SetGeneralOrAlternateOutput(GPIOA,11, OUTPUT_GPIO);
-	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOA,11, OUTPUT_PUSHPULL);
-	GPIO_ResetPin(GPIOA,11);
-	
-	GPIO_SetInputOrOutputMode(GPIOA,12, MODE_OUTPUT_2MHz);
-	GPIO_OutputMode_SetGeneralOrAlternateOutput(GPIOA,12, OUTPUT_GPIO);
-	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOA,12, OUTPUT_PUSHPULL);
-	GPIO_ResetPin(GPIOA,12);
-}
-
-//**************************************
-void ConfigureButtons(void){
-	BUS_GPIOA_EnableOrDisable(1); 
-	
-	GPIO_SetInputOrOutputMode(GPIOA,0, MODE_INPUT);
-	GPIO_InputMode_SetInputType(GPIOA,0, INPUT_PULLUP);
-	
-	GPIO_SetInputOrOutputMode(GPIOA,1, MODE_INPUT);
-	GPIO_InputMode_SetInputType(GPIOA,1,INPUT_PULLDOWN);
-	
-	GPIO_SetInputOrOutputMode(GPIOA,2, MODE_INPUT);
-	GPIO_InputMode_SetInputType(GPIOA,2,INPUT_PULLUP);
-}
-
-//**************************************
-void ConfigureRelays(void){
+void BcdInput_Config(void){
 	BUS_GPIOA_EnableOrDisable(1);
 	
-	GPIO_SetInputOrOutputMode(GPIOA,3, MODE_OUTPUT_2MHz);
-	GPIO_OutputMode_SetGeneralOrAlternateOutput(GPIOA,3, OUTPUT_GPIO);
-	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOA,3, OUTPUT_PUSHPULL);
-	GPIO_ResetPin(GPIOA,3);
+	GPIO_SetInputOrOutputMode(GPIOA,4, IO_INPUT);
+	GPIO_InputMode_SetInputType(GPIOA,4, INPUT_FLOATING);
 	
-	GPIO_SetInputOrOutputMode(GPIOA,4, MODE_OUTPUT_2MHz);
-	GPIO_OutputMode_SetGeneralOrAlternateOutput(GPIOA,4, OUTPUT_GPIO);
-	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOA,4, OUTPUT_PUSHPULL);
-	GPIO_ResetPin(GPIOA,4);
+	GPIO_SetInputOrOutputMode(GPIOA,5, IO_INPUT);
+	GPIO_InputMode_SetInputType(GPIOA,5, INPUT_FLOATING);
+	
+	GPIO_SetInputOrOutputMode(GPIOA,6, IO_INPUT);
+	GPIO_InputMode_SetInputType(GPIOA,6, INPUT_FLOATING);
+	
+	GPIO_SetInputOrOutputMode(GPIOA,7, IO_INPUT);
+	GPIO_InputMode_SetInputType(GPIOA,7	, INPUT_FLOATING);
+}
+
+//**************************************
+void BcdDisplay_Config(void){
+	BUS_GPIOB_EnableOrDisable(1);
+	
+	GPIO_SetInputOrOutputMode(GPIOB,12, IO_OUTPUT);
+	GPIO_OutputMode_SetGeneralPurposeOrAlternateFunction(GPIOB,12, OUTPUT_GP);
+	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOB,12, OUTPUT_PUSHPULL);
+	GPIO_ResetPin(GPIOB,12);
+	
+	GPIO_SetInputOrOutputMode(GPIOB,13, IO_OUTPUT);
+	GPIO_OutputMode_SetGeneralPurposeOrAlternateFunction(GPIOB,13, OUTPUT_GP);
+	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOB,13, OUTPUT_PUSHPULL);
+	GPIO_ResetPin(GPIOB,13);
+	
+	GPIO_SetInputOrOutputMode(GPIOB,14, IO_OUTPUT);
+	GPIO_OutputMode_SetGeneralPurposeOrAlternateFunction(GPIOB,14, OUTPUT_GP);
+	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOB,14, OUTPUT_PUSHPULL);
+	GPIO_ResetPin(GPIOB,14);
+	
+	GPIO_SetInputOrOutputMode(GPIOB,15, IO_OUTPUT);
+	GPIO_OutputMode_SetGeneralPurposeOrAlternateFunction(GPIOB,15, OUTPUT_GP);
+	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOB,15, OUTPUT_PUSHPULL);
+	GPIO_ResetPin(GPIOB,15);
+}
+
+//**************************************
+void Buttons_Config(void){
+	BUS_GPIOA_EnableOrDisable(1); 
+	
+	GPIO_SetInputOrOutputMode(GPIOA,0, IO_INPUT);
+	GPIO_InputMode_SetInputType(GPIOA,0, INPUT_PULLUP);
+	
+	GPIO_SetInputOrOutputMode(GPIOA,1, IO_INPUT);
+	GPIO_InputMode_SetInputType(GPIOA,1,INPUT_PULLDOWN);
+}
+
+//**************************************
+void LED_Config(void){
+	BUS_GPIOC_EnableOrDisable(1);
+	
+	GPIO_SetInputOrOutputMode(GPIOC,13, IO_OUTPUT);
+	GPIO_OutputMode_SetGeneralPurposeOrAlternateFunction(GPIOC,13, OUTPUT_GP);
+	GPIO_OutputMode_SetPushPullOrOpenDrain(GPIOC,13, OUTPUT_PUSHPULL);
+	GPIO_ResetPin(GPIOC,13);
 }
 
--- GPIO_BareMetal/UserCode/_SystemClock.h
+++ GPIO_BareMetal/UserCode/_SystemClock.h
@@ -1,12 +1,14 @@
 // GitHub Account: GitHub.com/AliRezaJoodi
+// It's a local library
 
-#include "Utility.h"
-#include "STM32F1xx_System_BareMetal.h"
-#include "STM32F1xx_RCC_BareMetal.h"
+#include <utility.h>
+#include <stm32f1xx_bm_system.h>
+#include <stm32f1xx_bm_rcc.h>
 
 //****************************************************
-void SystemClock_Configuration(void){
+void SystemClock_Config(void){
 	FLASH_SetLatency(LATENCY0);
+	
 	RCC_HSI_SetCalibTrimming(16);
 	RCC_HSI_EnableOrDisable(1);
 	RCC_SYSCLK_SetClockSource(SYSCLK_HSI);
