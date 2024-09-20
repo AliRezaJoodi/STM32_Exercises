7bc999a0700c16d7f8786f0a58669f9a1e6b8a42
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2024-09-20 14:55:23 +0330

    Update

--- Button/Core/Inc/_hardware_v1.0.h
+++ Button/Core/Inc/_hardware_v1.0.h
@@ -1,7 +1,7 @@
 #define BUTTON_HARDWARE
-	#define PRESSED             0
-  #define BUTTONLAG1          30        //ms 
-  #define BUTTONLAG2          10000      //Button Lag
+	#define BUTTON_PRESSED      0
+  #define BUTTON_LAG1         30        //ms 
+  #define BUTTON_LAG2         5000      //Button Lag
 	
 	#define BUTTON1_GPIO				GPIOA
   #define BUTTON1_PIN					0
