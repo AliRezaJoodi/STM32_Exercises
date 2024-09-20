0a38689ecae2be10f6a545be64cd669121711a12
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2024-09-20 14:36:40 +0330

    Initial Entry

---
+++ Button/Core/Inc/_hardware_v1.0.h
@@ -0,0 +1,13 @@
+#define BUTTON_HARDWARE
+	#define PRESSED             0
+  #define BUTTONLAG1          30        //ms 
+  #define BUTTONLAG2          10000      //Button Lag
+	
+	#define BUTTON1_GPIO				GPIOA
+  #define BUTTON1_PIN					0
+    
+  #define BUTTON2_GPIO				GPIOA
+  #define BUTTON2_PIN         1
+	
+	#define BUTTON3_GPIO				GPIOA
+  #define BUTTON3_PIN         2      
