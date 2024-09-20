32b793d15c1bf5f379b6243b9de42ef628c2af42
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2024-09-20 14:36:16 +0330

    Update

---
+++ _ExtraDrivers/Inc/button.h
@@ -0,0 +1,53 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#include <utility.h>
+#include <stm32f1xx_bm_bus.h>
+#include <stm32f1xx_bm_gpio.h>
+#include <delay.h>
+#include "_hardware_v1.0.h"
+
+#ifndef _BUTTON_INCLUDED
+    #define _BUTTON_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef BUTTON_HARDWARE
+#define BUTTON_HARDWARE
+	#define PRESSED             0
+  #define BUTTONLAG1          30        //ms 
+  #define BUTTONLAG2          10000      //Button Lag
+	
+	#define BUTTON1_GPIO				GPIOA
+  #define BUTTON1_PIN					0
+    
+  #define BUTTON2_GPIO				GPIOA
+  #define BUTTON2_PIN         1
+	
+	#define BUTTON3_GPIO				GPIOA
+  #define BUTTON3_PIN         2
+#endif
+
+void Button_Config(void);
+
+#ifdef BUTTON1_PIN
+	char Button1_OneStep(void);
+	char Button1_Continuous(void);
+#endif
+
+#ifdef BUTTON2_PIN
+	char Button2_OneStep(void);
+	char Button2_Continuous(void);
+#endif
+
+#ifdef BUTTON3_PIN
+	char Button3_OneStep(void);
+	char Button3_Continuous(void);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif		// _BUTTON_INCLUDED
---
+++ _ExtraDrivers/_test.h
@@ -0,0 +1,18 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#ifndef _TEST_INCLUDED
+    #define _TEST_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _TEST_HARDWARE
+#define _TEST_HARDWARE
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif		// _TEST_INCLUDED
