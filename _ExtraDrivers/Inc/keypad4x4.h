2f0393c3804929bddbcdda4f5a90aff96f2d9550
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2024-09-27 21:18:20 +0330

    Initial Entry

---
+++ _ExtraDrivers/Inc/keypad4x4.h
@@ -0,0 +1,23 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#include <stm32f1xx.h>
+#include <utility.h>
+#include <stm32f1xx_bm_bus.h>
+#include <stm32f1xx_bm_gpio.h>
+
+#ifndef _KEYPAD4X4_INCLUDED
+    #define _KEYPAD4X4_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef KEYPAD4X4_HARDWARE
+#define KEYPAD4X4_HARDWARE
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif		// _KEYPAD4X4_INCLUDED
