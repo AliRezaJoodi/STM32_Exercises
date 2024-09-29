6e2a38f5f0c3da2b892818b27fbc428ae63067b4
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2024-09-30 02:26:58 +0330

    Initial Entry

---
+++ Buzzer_BareMetal/Core/Inc/_hardware.h
@@ -0,0 +1,9 @@
+
+#define STM32F1XX_BM_TIMER_SYSTICK_HARDWARE
+	#define HCLK_VALUE		8000000
+
+#define BUZZER_BM_HARDWARE
+	#define BUZZER_ACTIVE       1     
+  #define BUZZER_PIN          1
+  #define BUZZER_GPIO         GPIOA
+	
---
+++ Buzzer_BareMetal/Core/Src/main.c
@@ -0,0 +1,26 @@
+// GitHub Account: GitHub.com/AliRezaJoodi
+
+#include <stm32f1xx.h>
+
+#include <utility.h>
+#include <stm32f1xx_bm_system.h>
+#include <stm32f1xx_bm_rcc.h>
+#include <stm32f1xx_bm_bus.h>
+#include <stm32f1xx_bm_gpio.h>
+#include <stm32f1xx_bm_timer_systick.h>
+#include <buzzer_bm.h>
+
+int main(void){
+	BUS_PWR_EnableOrDisable(1);
+	BUS_AFIO_EnableOrDisable(1);
+	GPIO_SWJ_SetDebugInterfaces(SWJ_SWD);
+  RCC_ConfigSystemClock();
+
+	Buzzer_Config();
+	Buzzer_MakeBeep(BEEP_SET);
+	
+  while(1){	
+  }
+}
+
+
