e7b04c89b1892eaba943f209938a6a0377ce1f01
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2024-11-05 05:57:55 +0330

    Update

--- _ExtraDrivers/Src/stm32f1xx_bm_rcc.c
+++ _ExtraDrivers/Src/stm32f1xx_bm_rcc.c
@@ -6,29 +6,34 @@
 void RCC_SystemClock_ConfigDefault1(void){
 	FLASH_SetLatency(LATENCY0);
 	
-	RCC_ADC_SetPrescaler(RCC_ADC_DIV2);
-	RCC_APB2_SetPrescaler(RCC_APB2_DIV1);
-	RCC_APB1_SetPrescaler(RCC_APB1_DIV1);
-	RCC_AHB_SetPrescaler(RCC_AHB_DIV1);
-	RCC_SeystemClock_SetSource(RCC_SYSCLK_CLKSOURCE_HSI);
 	RCC_HSI_SetTrimming(16);
 	RCC_HSI_EnableOrDisable(1);
+	
+	RCC_AHB_SetPrescaler(RCC_AHB_DIV1);
+	RCC_APB1_SetPrescaler(RCC_APB1_DIV1);
+	RCC_APB2_SetPrescaler(RCC_APB2_DIV1);
+	RCC_SeystemClock_SetSource(RCC_SYSCLK_CLKSOURCE_HSI);
+	
+	RCC_ADC_SetPrescaler(RCC_ADC_DIV2);
 }
 
 //****************************************************
 void RCC_SystemClock_ConfigDefault2(void){
 	FLASH_SetLatency(LATENCY2);
 	
-	RCC_ADC_SetPrescaler(RCC_ADC_DIV2);
-	RCC_APB2_SetPrescaler(RCC_APB2_DIV1);
-	RCC_APB1_SetPrescaler(RCC_APB1_DIV2);
-	RCC_AHB_SetPrescaler(RCC_AHB_DIV1);
-	RCC_SeystemClock_SetSource(RCC_SYSCLK_CLKSOURCE_PLL);
+	RCC_HSE_SetClockSource(RCC_HSE_CLKSOURCE_XTAL);
+	RCC_HSE_EnableOrDisable(1);
+	
 	RCC_PLL_SetMultiplicationFactor(RCC_PLL_MUL9);
 	RCC_PLL_SetClockSource(RCC_PLL_CLKSOURCE_HSE_DIV1);
-	RCC_HSE_SetClockSource(RCC_HSE_CLKSOURCE_XTAL);
 	RCC_PLL_EnableOrDisable(1);
-	RCC_HSE_EnableOrDisable(1);
+
+	RCC_AHB_SetPrescaler(RCC_AHB_DIV1);
+	RCC_APB1_SetPrescaler(RCC_APB1_DIV2);
+	RCC_APB2_SetPrescaler(RCC_APB2_DIV1);
+	RCC_SeystemClock_SetSource(RCC_SYSCLK_CLKSOURCE_PLL);
+
+	RCC_ADC_SetPrescaler(RCC_ADC_DIV2);
 }
 
 //****************************************************
