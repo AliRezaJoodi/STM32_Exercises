dde2ef2cfd2c45c69b7b4fb42f83cdb2ac184ce2
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2024-11-08 02:35:03 +0330

    Update

--- _ExtraDrivers/Inc/utility.h
+++ _ExtraDrivers/Inc/utility.h
@@ -14,16 +14,18 @@ extern "C" {
 #define ClearBit_NoLastStatus(ADDRESS, BIT) ADDRESS = ~(0b1UL<<(BIT));
 #define ToggleBit(ADDRESS, BIT)        			ADDRESS = (ADDRESS) ^ (0b1UL<<(BIT));
 #define ToggleData(ADDRESS)        					ADDRESS = ~(ADDRESS);
-#define WriteBit(ADDRESS, BIT, STATUS)			ADDRESS = ((ADDRESS) & (~(0b1UL<<(BIT)))) | (((STATUS)&0b1UL) << (BIT));
-#define Write2Bit(ADDRESS, FROM, VALUE)			ADDRESS = ((ADDRESS) & (~(0b11UL<<(FROM)))) | (((VALUE)&0b11UL) << (FROM));
-#define Write3Bit(ADDRESS, FROM, VALUE)			ADDRESS = ((ADDRESS) & (~(0b111UL<<(FROM)))) | (((VALUE)&0b111UL) << (FROM));
-#define Write4Bit(ADDRESS, FROM, VALUE)			ADDRESS = ((ADDRESS) & (~(0b1111UL<<(FROM)))) | (((VALUE)&0b1111UL) << (FROM));
-#define Write5Bit(ADDRESS, FROM, VALUE)			ADDRESS = ((ADDRESS) & (~(0b11111UL<<(FROM)))) | (((VALUE)&0b11111UL) << (FROM));
-#define GetBit(ADDRESS, BIT)           			(((ADDRESS) >> (BIT)) & 0b1UL) 
-#define Get2Bit(ADDRESS, FROM)           		(((ADDRESS) >> (FROM)) & 0b11UL)
-#define Get3Bit(ADDRESS, FROM)           		(((ADDRESS) >> (FROM)) & 0b111UL)
-#define Get4Bit(ADDRESS, FROM)           		(((ADDRESS) >> (FROM)) & 0b1111UL)
-#define Get5Bit(ADDRESS, FROM)           		(((ADDRESS) >> (FROM)) & 0b11111UL)
+
+#define WriteBit(ADDRESS, FROM, VALUE)			ADDRESS = ((ADDRESS) & (~(0b1UL<<(FROM)))) 			| ((0b1UL&(VALUE)) << (FROM));
+#define Write2Bit(ADDRESS, FROM, VALUE)			ADDRESS = ((ADDRESS) & (~(0b11UL<<(FROM)))) 		| ((0b11UL&(VALUE)) << (FROM));
+#define Write3Bit(ADDRESS, FROM, VALUE)			ADDRESS = ((ADDRESS) & (~(0b111UL<<(FROM)))) 		| ((0b111UL&(VALUE)) << (FROM));
+#define Write4Bit(ADDRESS, FROM, VALUE)			ADDRESS = ((ADDRESS) & (~(0b1111UL<<(FROM)))) 	| ((0b1111UL&(VALUE)) << (FROM));
+#define Write5Bit(ADDRESS, FROM, VALUE)			ADDRESS = ((ADDRESS) & (~(0b11111UL<<(FROM)))) 	| ((0b11111UL&(VALUE)) << (FROM));
+
+#define GetBit(ADDRESS, FROM)           		(((ADDRESS) >> (FROM)) 	& 0b1)
+#define Get2Bit(ADDRESS, FROM)           		(((ADDRESS) >> (FROM)) 	& 0b11)
+#define Get3Bit(ADDRESS, FROM)           		(((ADDRESS) >> (FROM)) 	& 0b111)
+#define Get4Bit(ADDRESS, FROM)           		(((ADDRESS) >> (FROM)) 	& 0b1111)
+#define Get5Bit(ADDRESS, FROM)           		(((ADDRESS) >> (FROM)) 	& 0b11111)
 
 #ifdef __cplusplus
 }
