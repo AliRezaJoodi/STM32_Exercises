700ee3d3bf20d4b26b7ee3c66dfcb568ed765489
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2024-11-14 03:33:13 +0330

    Initial Entry

---
+++ ADC1_SingleModeWithScan_3CH_BareMetal/Core/Inc/main.h
@@ -0,0 +1,36 @@
+
+#ifndef _MAIN_INCLUDED
+#define _MAIN_INCLUDED
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stm32f1xx.h>
+
+#ifndef HCLK_VALUE
+	#define HCLK_VALUE			8000000
+#endif
+
+#ifndef PCLK1_VALUE
+	#define PCLK1_VALUE			8000000
+#endif
+
+#ifndef PCLK2_VALUE
+	#define PCLK2_VALUE			8000000
+#endif
+
+#ifndef TIMEOUT_VALUE
+	#define TIMEOUT_VALUE		1000000
+#endif
+
+#ifndef DELAY_NOP_LAG
+	#define DELAY_NOP_LAG		250000
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif		// _MAIN_INCLUDED
