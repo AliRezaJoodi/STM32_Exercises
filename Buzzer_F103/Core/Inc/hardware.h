6e76c91d70292e81ed1ac8e94f57bfc0e693ecf9
Ali Reza Joodi <AliRezaJoodi@outlook.com>
2026-07-10 21:03:45 +0330

    chore: update

--- Buzzer_F103/Core/Inc/hardware.h
+++ Buzzer_F103/Core/Inc/hardware.h
@@ -5,11 +5,12 @@
     #define AJ_TARGET_MCU_BITS     32U      /*!< Supported: 8, 16, 32 */
     #define AJ_TARGET_TICK_BITS    32U      /*!< Supported: 8, 16, 32 */
 
-#define AJ_BUZZER_HARDWARE
+#define AJ_BUZZER_CONFIG
     #define AJ_BUZZER_ACTIVE       0U
     #define AJ_BUZZER_COUNT1       75U
     #define AJ_BUZZER_COUNT2       200U
 
+#define AJ_BUZZER_CONFIG_PLATFORM
     #define AJ_BUZZER_GPIO         GPIOC
     #define AJ_BUZZER_POS          13U
     #define AJ_BUZZER_MASK         (1U << AJ_BUZZER_POS)
--- Buzzer_F401/Core/Inc/hardware.h
+++ Buzzer_F401/Core/Inc/hardware.h
@@ -1,17 +1,20 @@
 #ifndef HARDWARE_INCLUDED
 #define HARDWARE_INCLUDED
 
+
 #define AJ_TARGET_CONFIG
     #define AJ_TARGET_MCU_BITS     32U      /*!< Supported: 8, 16, 32 */
     #define AJ_TARGET_TICK_BITS    32U      /*!< Supported: 8, 16, 32 */
 
-#define AJ_BUZZER_HARDWARE
+#define AJ_BUZZER_CONFIG
     #define AJ_BUZZER_ACTIVE       1U
     #define AJ_BUZZER_COUNT1       75U
     #define AJ_BUZZER_COUNT2       200U
 
+#define AJ_BUZZER_CONFIG_PLATFORM
     #define AJ_BUZZER_GPIO         GPIOC
     #define AJ_BUZZER_POS          13U
     #define AJ_BUZZER_MASK         (1U << AJ_BUZZER_POS)
+
 
 #endif
